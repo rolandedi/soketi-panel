2d225b8fe9b9283d21801e861cc911072c9b9f2d
Roland Edi <rolandedi@codivoire.com>
2026-03-24 22:55:15 +0000

    build(deps): ajouter config ESLint et dépendances
    Ajout d'un fichier de configuration ESLint dédié et activation du module
    @nuxt/eslint dans nuxt.config.ts. Le script `typecheck` est ajouté ainsi que
    de nouvelles dépendances (VueUse, better-auth, date-fns, knex, pusher,
    reka-ui, vue-sonner, zod) et des devDependencies TypeScript. Ces changements
    préparent le projet à la vérification de type et améliorent la qualité du
    code.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+
+  typescript: {
+    typeCheck: true,
+  },
+
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint"],
+});
