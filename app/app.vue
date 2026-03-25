5362dafb6cc22dd33748e2344d9d424155e12b7c
Roland Edi <rolandedi@codivoire.com>
2026-03-25 00:22:40 +0000

    feat(ui): ajouter titre dynamique à la page
    Ajoute un script setup utilisant `useHead` pour définir `titleTemplate`, permettant d’afficher le titre de chaque page suivi de « - Soketi Panel ». Cette amélioration améliore la cohérence des titres et l’expérience utilisateur.

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,11 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Soketi Panel` : "Soketi Panel";
+  },
+});
+</script>
