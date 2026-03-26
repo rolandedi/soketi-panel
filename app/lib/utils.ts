f61e090bce6294456cfc735fde1cd5ea8cd12f33
Roland Edi <rolandedi@codivoire.com>
2026-03-26 16:16:01 +0000

    refactor(utils): supprimer fonction getInitials inutilisée
    Supprime la fonction `getInitials` qui n’était plus utilisée dans le projet, afin de nettoyer le module utilitaire et réduire la surface du code.
    
    BREAKING CHANGE: la fonction exportée `getInitials` a été supprimée, ce qui peut entraîner des erreurs d’importation dans les modules qui l’utilisaient.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -5,12 +5,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const getInitials = (name: string) => {
-  return name
-    .split(" ")
-    .map((n) => n[0])
-    .join("")
-    .toUpperCase()
-    .substring(0, 2);
-};
